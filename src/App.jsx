daee3206584c83b1481dce270ca64253ba083857
Ypzar John <ypzarjohn24@gmail.com>
2024-07-31 12:42:39 +0800

    Adding About component in the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
+import About from "./components/About/About";
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <About />
     </>
   );
 }
