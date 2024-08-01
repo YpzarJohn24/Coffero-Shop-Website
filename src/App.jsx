4e2d30e7252f97911e34772d12002426273aed8a
Ypzar John <ypzarjohn24@gmail.com>
2024-08-01 11:25:20 +0800

    Adding Features component to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Features from "./components/Features/Features";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Header />
       <Home />
       <About />
+      <Features />
     </>
   );
 }
